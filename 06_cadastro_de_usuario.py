b0ddef855f39f21bf9d163e85ad52788a2f532b9
Samuel Silva <samuel61924766@edu.pa.senac.br>
2026-04-10 20:54:10 -0300

    cadastro_de_usuario_lab6
    Estou aprendendo agora como que faz o meu cadastro único, caso eu precise de um pra me matricular na escola da vida

--- 06_cadastro_de_usuario.py
+++ 06_cadastro_de_usuario.py
@@ -3,25 +3,32 @@ O sistema deve exibir as informações do usuário em um texto amigável.'''
 
 
 
-print("para fazermos o seu cadastro, digite as seguintes informações:")
-var_nome = input("digite o seu nome: ")
-var_sexo = input("digite seu sexo: ")
-var_endereço = input("digite o seu endereço: ")
-var_cidade = input("digite sua cidade: ")
-var_estado = input("digite o seu estado: ")
-var_cep = input("digite seu cep: ")
-var_telefone = input("digite seu telefone: ")
-var_nascimento = input("digite sua data de nascimento: ")
-var_rg = input("digite seu RG: ")
-var_nomepai = input("digite o nome do seu pai: ")
-var_nomemae = input("digite o nome da sua mãe: ")
-var_grauescolar = input("digite seu grau de escolaridade: ")
-
-print("nome do usuario: ")(var_nome)
-print
-print
-print
-print
-print
+info_necss = ["nome"]
+info = []
 
 
+
+for i in info_necss:
+    dado = input('digite o seu',i)
+    info.append(dado)
+    
+
+print("Para fazermos o seu cadastro, digite as seguintes informações:")
+
+# var_nome = input("digite o seu nome: ")
+# var_sexo = input("digite seu sexo: ")
+# var_endereço = input("digite o seu endereço: ")
+# var_cidade = input("digite sua cidade: ")
+# var_estado = input("digite o seu estado: ")
+# var_cep = input("digite seu cep: ")
+# var_telefone = input("digite seu telefone: ")
+# var_nascimento = input("digite sua data de nascimento: ")
+# var_rg = input("digite seu RG: ")
+# var_nomepai = input("digite o nome do seu pai: ")
+# var_nomemae = input("digite o nome da sua mãe: ")
+# var_grauescolar = input("digite seu grau de escolaridade: ")
+
+# print(f'usuario:{var_nome},do sexo:{var_sexo},morador do endereço:{var_endereço},no estado:{var_estado}')
+      
+""" {var_cep}, tel{var_telefone}\n nascimento:{var_nascimento}, Rg{var_rg}\n Filiado:{var_nomepai} e {var_nomemae}\n Grau escolar:{var_grauescolar})"""
+      
