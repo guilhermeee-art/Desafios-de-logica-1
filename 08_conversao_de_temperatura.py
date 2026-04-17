bff929ce27dda9463784c7da9e2883b5aba985f6
Samuel Silva <samuel61924766@edu.pa.senac.br>
2026-04-17 20:21:43 -0300

    fahrenheit resoluçao

--- 08_conversao_de_temperatura.py
+++ 08_conversao_de_temperatura.py
@@ -3,3 +3,8 @@ Para sua surpresa o termômetro estava em Fahrenheit.
 Desenvolva um sistema que recebe a temperatura em Fahrenheit e responda a quantos graus equivale em Celsius.
 Fórmula de conversão: ºC = (ºF-32)/1.8
 '''
+
+
+fahrenheit = int (input ("Digite a temperatura em fahrenheit: "))
+divisao = fahrenheit-32
+print (f"A Temperatura em Celsius é:{divisao/1.8}")
--- 09_quantos_passos_ate_a_escola.py
+++ 09_quantos_passos_ate_a_escola.py
@@ -3,3 +3,5 @@ Para isso ela mediu quantos passos ela caminha em um metro e quantos metros há 
 Desenvolva um sistema que receba as informações de quantos passos Ane precisa andar para caminhar um metro, 
 quantos metros ela anda e calcule quanto passos ela precisa caminhar até chegar ao Senac.
 '''
+
+metro = input("quantos passos voce anda em um metro: ")
