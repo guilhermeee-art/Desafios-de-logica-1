29162b245329b81af0759455af19991a53908785
Samuel Silva <samuel61924766@edu.pa.senac.br>
2026-04-17 20:44:33 -0300

    ae

--- janelas.py
+++ janelas.py
@@ -9,7 +9,8 @@ frame.place(relx=0.0,rely=0.0, relwidth=0.2, relheight=1)
 frame2 = tk.Frame(janela, bg="yellow") 
 frame2.place(relx=0.2 , rely=0.8, relwidth=1 , relheight=0.2)
 
-
+frame3 = tk.Frame(janela, bg="green")
+frame3.place(relx=0.2 , rely=0.0, relwidth=1, relheight=0.1) 
 
 
 
